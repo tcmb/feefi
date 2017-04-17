00a951071d9a062dce49e537e10bf9ed644fde36
Florian Ilgenfritz <florian@newstore.com>
2017-04-17 21:56:21 +0200

    completing oauth token exchange

--- constants.py
+++ constants.py
@@ -1,4 +1,7 @@
-from secrets import REMOTEHOST
+from stravalib.attributes import LatLon
 
-STRAVA_OAUTH_URL = 'https://www.strava.com/oauth/authorize'
+STRAVA_AUTH_URL = 'https://www.strava.com/oauth/authorize'
+STRAVA_TOKEN_URL = 'https://www.strava.com/oauth/token'
 LOCALHOST = 'http://127.0.0.1:5000/'
+P = 0.017453292519943295
+HOME_LOC = LatLon(lat=52.47, lon=13.45)
--- follow_me.py
+++ follow_me.py
@@ -1,7 +1,13 @@
 from flask import Flask, redirect, request, render_template
-from constants import STRAVA_OAUTH_URL, LOCALHOST
+from constants import *
 from secrets import *
 from urllib import urlencode
+import requests
+import json
+
+from stravalib.client import Client
+from helpers import matches_criteria, get_min_dist, km
+
 
 app = Flask(__name__)
 
@@ -13,14 +19,26 @@ my_app_params = {
     'scope': 'public'
 }
 
+
+def get_user_token(authorization_code):
+    params = {
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'code': authorization_code
+    }
+    user_token_response = requests.post(STRAVA_TOKEN_URL, data=params)
+    returned_data = json.loads(user_token_response.text)
+    user_token = returned_data.get('access_token')
+    return user_token
+
+
 @app.route('/')
 def authoriziation_redirect():
-    # print 'Method: %s\nForm: %s\nArgs: %s\nEOM' % (
-    #     request.method, request.form, request.args
-    # )
     authorization_code = request.args.get('code')
     if authorization_code:
-        return render_template('index.html', authorization_code=authorization_code)
+        user_token = get_user_token(authorization_code)
+        client = Client(user_token)
+        return render_template('index.html', user_token=user_token)
     else:
-        redirect_url = STRAVA_OAUTH_URL + '?' + urlencode(my_app_params)
+        redirect_url = STRAVA_AUTH_URL + '?' + urlencode(my_app_params)
         return redirect(redirect_url)
