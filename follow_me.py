3539f83fe12787388f2a605e636166de7c2bb8f7
Florian Ilgenfritz <florian@newstore.com>
2017-04-17 22:11:14 +0200

    separating urls for initial request and authorized redirect

--- follow_me.py
+++ follow_me.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, request, render_template
 from constants import *
 from secrets import *
 from urllib import urlencode
+from urlparse import urljoin
 import requests
 import json
 
@@ -10,14 +11,18 @@ from helpers import matches_criteria, get_min_dist, km
 
 
 app = Flask(__name__)
+my_host = LOCALHOST
+#my_host = REMOTEHOST
 
 my_app_params = {
     'client_id': CLIENT_ID,
-    'redirect_uri': LOCALHOST,
+    'redirect_uri': urljoin(my_host, '/authorized/'),
     'response_type': 'code',
     'approval_prompt': 'auto',
     'scope': 'public'
 }
+
+authorization_url = STRAVA_AUTH_URL + '?' + urlencode(my_app_params)
 
 
 def get_user_token(authorization_code):
@@ -34,11 +39,31 @@ def get_user_token(authorization_code):
 
 @app.route('/')
 def authoriziation_redirect():
+    """
+    Redirect the user to the Strava authorization page.
+    Assumptions:
+    - User has not authorized this app yet
+    - User has a Strava account
+    (- User will authorize this app and return)
+    """
+    return redirect(authorization_url)
+
+
+@app.route('/authorized/')
+def authorized():
+    """
+    Run activity search for authorized user.
+    Assumptions:
+    - User has authorized this app
+    - User agent is being sent from Strava oauth redirect back to us
+    - Token exchange will succeed
+    (- Getting activities will work)
+    """
     authorization_code = request.args.get('code')
     if authorization_code:
         user_token = get_user_token(authorization_code)
         client = Client(user_token)
         return render_template('index.html', user_token=user_token)
     else:
-        redirect_url = STRAVA_AUTH_URL + '?' + urlencode(my_app_params)
-        return redirect(redirect_url)
+        #return 400
+        pass
