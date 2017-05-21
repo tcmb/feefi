51a58c3d81c35a767561eef6ab9a31aa82a006a1
Florian Ilgenfritz <florian@newstore.com>
2017-05-21 10:42:43 +0200

    extracting custom css and js into separate files, adding offline mode switch, adding lipsum in preparation for some layout

---
+++ static/js/feefi.js
@@ -0,0 +1,16 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidGNtYiIsImEiOiJjajJ4am9hczAwMDdpMndubzhyMDNkOXpzIn0.9HuZArmMyqiP-QzCY2diGA';
+
+if (!document.offlineMode) {
+
+    var map = new mapboxgl.Map({
+        container: 'map-one', // container id
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [13.45, 52.47], // starting position
+        zoom: 11 // starting zoom
+    });
+
+    map.on('click', function (e) {
+        document.getElementById("home_loc_lat").value = e.lngLat.lat.toFixed(2);
+        document.getElementById("home_loc_lon").value = e.lngLat.lng.toFixed(2);
+    });
+}
